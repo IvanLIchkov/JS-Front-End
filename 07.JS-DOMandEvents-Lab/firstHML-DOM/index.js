874dbf0cff66191a21387aee4fe0405daf7986ae
Scopi1 <Vanko.lichkov@abv.bg>
2023-03-06 11:01:28 +0200

    first dom updates

---
+++ 07.JS-DOMandEvents-Lab/firstHML-DOM/index.js
@@ -0,0 +1,10 @@
+function chancgeText(){
+    let header = document.getElementsByTagName('h1');
+
+    let input = document.getElementById(`my-data`);
+
+    let value = input.value;
+
+    header[0].innerHTML = `<p>${value}</p>`;
+    header[0].style.color = 'red';
+};
